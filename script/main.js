110c6fdf42367d38f28a635f77afe731f03dd3a9
qrulleau <quentin.rulleau@yahoo.com>
2023-09-14 15:06:39 +0200

    ADD: creation grid fonctionnality

--- script/main.js
+++ script/main.js
@@ -6,32 +6,37 @@ buttonChoiceDifficulty.addEventListener("change", () => {
 
 const difficultySettings = {
   easy : {
+    "numberBomb" : 10,
+    "columnGrid" : 10,
+    "RowGrid" : 10,
+  },
+  medium : {
     "numberBomb" : 20,
     "columnGrid" : 20,
     "RowGrid" : 20,
   },
-  medium : {
+  hard : {
     "numberBomb" : 40,
     "columnGrid" : 40,
     "RowGrid" : 40,
-  },
-  hard : {
-    "numberBomb" : 60,
-    "columnGrid" : 60,
-    "RowGrid" : 60,
   }
 }
 
 function createGrid(difficulty){
+  console.log(Math.floor(Math.random() * 10));
   const main = document.querySelector('.main')
-  console.log(difficulty)
-  for (let index = 0; index < difficulty.columnGrid; index++) {
-    columnGrid = document.createElement('div')
-    main.appendChild(columnGrid);
-    console.log("bonjour")
-    
+  for (let index = 0; index < difficulty.RowGrid; index++) {
+    rowGrid = document.createElement('div')
+    rowGrid.classList.add('row');
+
+    for (let index = 0; index < difficulty.columnGrid; index++) {
+      caseGrid = document.createElement('div')
+      caseGrid.classList.add('case');
+      main.appendChild(rowGrid);
+      rowGrid.appendChild(caseGrid);
+      
+    } 
   }
-  
 }
 createGrid(difficultySettings.easy)
 
