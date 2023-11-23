b546e0469c75faa882d4197d2db89152cf290823
qrulleau <quentin.rulleau@yahoo.com>
2023-11-23 19:16:18 +0100

    ADD: update project

--- script/main.js
+++ script/main.js
@@ -1,24 +1,23 @@
 let buttonChoiceDifficulty = document.querySelector('select')
-
-buttonChoiceDifficulty.addEventListener("change", () => {
-  console.log(buttonChoiceDifficulty.value)
-})
+let casesGrid = document.querySelectorAll('.case');
+let buttonDifficulty = document.querySelector('#difficulty');
+let valueButtonDifficulty = buttonDifficulty.value
 
 const difficultySettings = {
   easy : {
-    "nbrBomb" : 10,
+    "nbrBomb" : 14,
     "nbrColumnGrid" : 10,
     "nbrRowGrid" : 10,
   },
   medium : {
-    "nbrBomb" : 20,
-    "nbrColumnGrid" : 20,
-    "nbrRowGrid" : 20,
+    "nbrBomb" : 28,
+    "nbrColumnGrid" : 15,
+    "nbrRowGrid" : 15,
   },
   hard : {
     "nbrBomb" : 40,
-    "nbrColumnGrid" : 40,
-    "nbrRowGrid" : 40,
+    "nbrColumnGrid" : 20,
+    "nbrRowGrid" : 20,
   }
 }
 
@@ -27,28 +26,34 @@ function createGrid(difficulty){
   for (let indexRow = 0; indexRow < difficulty.nbrRowGrid; indexRow++) {
     nbrRowGrid = document.createElement('div')
     nbrRowGrid.classList.add('row');
-
+    
     for (let index = 0; index < difficulty.nbrColumnGrid; index++) {
-      caseGrid = document.createElement('div')
+      let caseGrid = document.createElement('div')
       caseGrid.classList.add('case','hidden');
       caseGrid.setAttribute('id', `${indexRow}.${index}`);
       main.appendChild(nbrRowGrid);
       nbrRowGrid.appendChild(caseGrid);
-      
     } 
   }
 }
 
-function fillGrid(nbrBomb,nbrRowGrid,nbrColumnGrid){
-  
+function putNumberCase(){
+  let casesGrid = document.querySelectorAll('.case');
+  casesGrid.forEach(caseGrid => {
+    console.log(caseGrid);
+  })
+}
+
+function fillGrid(difficulty){
+  putNumberCase()
   let bomb = {
     src: '/assets/bomb.png'
   }
 
- for (let index = 0; index < nbrBomb; index++) {
+ for (let index = 0; index < difficulty.nbrBomb; index++) {
 
-  const aleaRow = Math.floor(Math.random() * nbrRowGrid);
-  const aleaColumn = Math.floor(Math.random() * nbrColumnGrid);
+  const aleaRow = Math.floor(Math.random() * difficulty.nbrRowGrid);
+  const aleaColumn = Math.floor(Math.random() * difficulty.nbrColumnGrid);
   let casse = document.getElementById(`${aleaRow}.${aleaColumn}`);
   let checkBomb = casse.getElementsByTagName('img');
 
@@ -69,7 +74,6 @@ function createBomb(casse,bomb){
   casse.appendChild(bombGrid);
 }
 
-createGrid(difficultySettings.easy)
 
 function reveal(){
   let casesGrid = document.querySelectorAll('.case');
@@ -87,6 +91,15 @@ function reveal(){
 }
 
 
-
+buttonChoiceDifficulty.addEventListener("change", () => {
+  let rows = document.querySelectorAll('.row');
+  rows.forEach(row => {
+    row.remove();
+  })
+  createGrid(difficultySettings[buttonChoiceDifficulty.value])
+  fillGrid(difficultySettings[buttonChoiceDifficulty.value])
+  reveal();
+})
+createGrid(difficultySettings[buttonChoiceDifficulty.value])
+fillGrid(difficultySettings[buttonChoiceDifficulty.value])
 reveal();
-fillGrid(difficultySettings.easy.nbrBomb, difficultySettings.easy.nbrRowGrid, difficultySettings.easy.nbrColumnGrid)
